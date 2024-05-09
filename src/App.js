aeb0f33d0209190f8a4415a7dba9a8feab462fb7
mhmtkarakas <m.karakas32@gmail.com>
2024-05-09 16:47:08 +0300

    useEffect integration

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import TaskCreate from "./components/TaskCreate";
 import TaskList from "./components/TaskList";
+import axios from "axios";
 
 function App() {
   const [tasks, setTasks] = useState([]);
-
-  const createTask = (title, desc) => {
+// axios post islemi yapilir
+  const createTask =async (title, desc) => {
+   const response =await axios.post("http://localhost:3004/posts",{
+    id: String(new Date().getTime()),
+    title,
+    desc,
+   });
+  // console.log(response);
     let createTasks = [
       ...tasks,
-      {
-        id: String(new Date().getTime()),
-        title,
-        desc,
-      },
+      response.data
     ];
     setTasks(createTasks);
   };
-
-  const onDeleteById = (id) => {
+  // axios get islemi yapilir
+  const fetchTask = async() =>{
+    const response = await axios.get("http://localhost:3004/posts");
+    console.log(response);
+    setTasks(response.data);
+  }
+  useEffect(()=>{
+    fetchTask();
+  },[])
+  // delete islemini burada yapariz.
+  const onDeleteById = async (id) => {
+     await axios.delete(`http://localhost:3004/posts/${id}`);
     const deleteProcess = tasks.filter((task) => task.id !== id);
     setTasks(deleteProcess);
   };
   // Guncellem islemini yapacagimiz metodumuzda gonderdigimiz parametreleri karsilariz.
-  const editTaskById = (id, updatedTask, updatedTaskDesc) => {
+  const editTaskById =async (id, updatedTask, updatedTaskDesc) => {
+  await axios.put(`http://localhost:3004/posts/${id}`,{
+    title:updatedTask,
+    desc: updatedTaskDesc
+  });
     // Guncelleme isleminde arrayin icindeki elemanlari tek tek donecegiz sonra bana gelen id degerine gore guncelleyecegimiz elemani bulacagiz.
     // Buldugumuz elemani parametreler ile yeni gelen baslik ve desc ile degistirecegiz ve elemanlari yeniden tek tek donecegiz ve yeni bir array e atayacagiz.
     const updatedTasks = tasks.map((task) => {
